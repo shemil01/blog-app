c5dd1869711b118b90a9820cabdb46c6ae41e198
shemil01 <shemilmk1033@gmail.com>
2024-10-07 20:08:20 +0530

    updated

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -40,11 +40,31 @@ const NavBar = () => {
         <SheetTrigger> <HamburgerMenuIcon className="size-7 md:hidden" /></SheetTrigger>
         <SheetContent>
           <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            {/* <SheetTitle>Are you absolutely sure?</SheetTitle>
             <SheetDescription>
               This action cannot be undone. This will permanently delete your
               account and remove your data from our servers.
-            </SheetDescription>
+            </SheetDescription> */}
+            <div className="flex flex-col " >
+            <ul className="space-y-10">
+        <li>
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"/about"}>About</Link>
+        </li>
+        <li>
+          <Link href={"/blog"}>Blog</Link>
+        </li>
+        <li>
+          <Link href={"/contact"}>Contact</Link>
+        </li>
+        <li className="buttons space-x-3 p-2">
+          <Button variant="outline">login</Button>
+        </li>
+          <Button variant="outline">Sign up</Button> 
+      </ul>
+            </div>
           </SheetHeader>
         </SheetContent>
       </Sheet>
